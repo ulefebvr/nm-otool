f597a44e7716587c6ebb8a683f2d5a9c2964ea61
ulefebvr <ulefebvr@student.42.fr>
2017-02-24 19:23:49 +0200

    [LIB] Fix minor bug strcmp

--- lib/libft/srcs/ft_strcmp.c
+++ lib/libft/srcs/ft_strcmp.c
@@ -12,12 +12,16 @@
 
 #include "libft.h"
 
-int		ft_strcmp(const char *s1, const char *s2)
+int		ft_strcmp(const char *p1, const char *p2)
 {
-	int counter;
-
-	counter = 0;
-	while (s1[counter] == s2[counter] && s1[counter])
-		counter++;
-	return ((unsigned char)s1[counter] - (unsigned char)s2[counter]);
+	if (0 == p1 || 0 == p2)
+		return (-1);
+	while ((unsigned char)*p1 == (unsigned char)*p2)
+	{
+		if ((unsigned char)*p1 == '\0')
+			return ((unsigned char)*p1 - (unsigned char)*p2);
+		++p1;
+		++p2;
+	}
+	return ((unsigned char)*p1 - (unsigned char)*p2);
 }
